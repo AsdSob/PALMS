eb834ab5cb660dcef7ae3f0519290b306c31ca6f
SobirASD <reksar_08@mail.ru>
2017-12-28 11:14:34 +0400

    Test of View Changer with errors

--- PALMS/PALMS.ViewModels/MenuViewModel.cs
+++ PALMS/PALMS.ViewModels/MenuViewModel.cs
@@ -4,7 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace PALMS.ViewModels
 {
@@ -26,16 +28,64 @@ namespace PALMS.ViewModels
             set { Set(() => SelectedItem, ref _selectedItem, value); }
         }
 
+        //public MenuViewModel()
+        //{
+        //    Items = new ObservableCollection<MenuItemViewModel> {
+
+        //        new MenuItemViewModel { Name = "1" },
+        //        new MenuItemViewModel { Name = "2" },
+        //        new MenuItemViewModel { Name = "3" },
+
+        //    };
+        //}
+
+
+        #region Test view changer
+
+        
+        private ViewModelBase _currentViewModel;
+
+        readonly static ClientViewModel _clientViewModel = new ClientViewModel();
+        readonly static MasterLinenViewModel _masterViewModel = new MasterLinenViewModel();
+        
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                if (_currentViewModel == value)
+                    return;
+                _currentViewModel = value;
+                RaisePropertyChanged("Content");
+            }
+        }
+
+      
+        public ICommand MasterViewCommand { get; private set; }
+
+        public ICommand ClientViewCommand { get; private set; }
+
+       
         public MenuViewModel()
         {
-            Items = new ObservableCollection<MenuItemViewModel> {
-
-                new MenuItemViewModel { Name = "1" },
-                new MenuItemViewModel { Name = "2" },
-                new MenuItemViewModel { Name = "3" },
-
-            };
+            CurrentViewModel = MenuViewModel._masterViewModel;
+            MasterViewCommand = new RelayCommand(() => ExecuteMasterViewCommand());
+            ClientViewCommand = new RelayCommand(() => ExecuteClientViewCommand());
         }
+
+        
+        private void ExecuteMasterViewCommand()
+        {
+            CurrentViewModel = MenuViewModel._masterViewModel;
+        }
+
+        
+        private void ExecuteClientViewCommand()
+        {
+            CurrentViewModel = MenuViewModel._clientViewModel;
+        }
+
+        #endregion
 
     }
 }
